cb0e63574b80a7a6577b51994aa74dd6510cbbdb
Lyohha <325alex.000@gnail.com>
2025-03-21 16:38:06 +0200

    Init commit

---
+++ csv.js
@@ -0,0 +1,85 @@
+document.querySelector('[js-csv-form]').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let $this = this;
+
+    let $text = $this.querySelector('textarea');
+    let text = $text.value;
+
+    let lines = text.split('\n');
+
+    let jsonArray = {};
+
+    lines.forEach(function(line, index) {
+        let columns = line.split(';');
+        if(columns.length == 1)
+            columns = line.split(',');
+        if(columns.length < 7)
+            return;
+
+        let i = 2;
+
+        let name = columns[0].replaceAll(' ', '').trim().toLowerCase() + columns[1].trim().replaceAll(' ', '').toLowerCase();
+
+        // console.log(name);
+
+        let id = md5(name);
+
+        let user = {
+            "group": "",
+            "spec": "",
+            "semesters": []
+        }
+
+        // console.log(columns);
+
+        for(let i = 2; i < columns.length; i += 5) {
+            let number = parseInt(columns[i]);
+            if(user.semesters.length < number) {
+                user.semesters.push([
+                    {
+                        "credits": columns[i + 1],
+                        "name": columns[i + 3],
+                        "type": columns[i + 2],
+                        "grade": parseInt(columns[i + 4])
+                    }
+                ]);
+                continue;
+            }
+
+            // console.log(number);
+
+            user.semesters[number - 1].push({
+                "credits": columns[i + 1],
+                "name": columns[i + 3],
+                "type": columns[i + 2],
+                "grade": parseInt(columns[i + 4])
+            });
+        }
+        if(user.semesters.length > 0)
+            jsonArray[id] = user;
+
+        // let obj = {
+        //     "name": columns[0],
+        //     "ukr": parseFloat(columns[1].replace(',', '.')),
+        //     "math": parseFloat(columns[2].replace(',', '.')),
+        //     "history": parseFloat(columns[3].replace(',', '.')),
+        //     "geo": parseFloat(columns[9].replace(',', '.')),
+        //     "eng": parseFloat(columns[4].replace(',', '.')),
+        //     "chem": parseFloat(columns[7].replace(',', '.')),
+        //     "phis": parseFloat(columns[6].replace(',', '.')),
+        //     "bio": parseFloat(columns[5].replace(',', '.')),
+        //     "lit": parseFloat(columns[8].replace(',', '.')),
+        //     // "con": columns[0],
+        //     // "coef": columns[0],
+        // };
+
+        // if(columns[10].length > 0) 
+        //     obj.con =  parseFloat(columns[10].replace(',', '.'));
+        // if(columns[11].length > 0) 
+        //     obj.coef =  parseFloat(columns[11].replace(',', '.'));
+
+        // jsonArray.push(obj);
+    });
+
+    $text.value = JSON.stringify(jsonArray);
+});
---
+++ main.js
@@ -0,0 +1,122 @@
+console.log("Grade app start")
+
+$(document).ready(function() {
+    function fillTable(student) {
+        let $table = $('.view-table');
+
+        let crossNumber = 0;
+
+        student.semesters.forEach(function(value, index) {
+            let semester = index + 1;
+
+            let year = 0;
+
+            if(semester == 1 || semester == 2) year = 1;
+            if(semester == 3 || semester == 4) year = 2;
+            if(semester == 5 || semester == 6) year = 3;
+            if(semester == 7 || semester == 8) year = 4;
+
+            let $semester = $(`<tr><td colspan="7" class="table-year">${year} курс, ${semester} семестр</td></tr>`);
+            $table.append($semester);
+
+            let grade = 0;
+
+            value.forEach(function(obj, j) {
+                crossNumber++;
+
+                grade += obj.grade;
+
+                let grade2 = '';
+                let grade3 = '';
+
+                if(obj.grade < 60) {
+                    grade2 = 'не задовільно';
+                    grade3 = 'F';
+                }
+                else if(obj.grade < 64) {
+                    grade2 = 'задовільно';
+                    grade3 = 'E';
+                }
+                else if(obj.grade < 74) {
+                    grade2 = 'задовільно';
+                    grade3 = 'D';
+                }
+                else if(obj.grade < 82) {
+                    grade2 = 'добре';
+                    grade3 = 'С';
+                }
+                else if(obj.grade < 90) {
+                    grade2 = 'добре';
+                    grade3 = 'B';
+                }
+                else {
+                    grade2 = 'відмінно';
+                    grade3 = 'А';
+                }
+
+                if(obj.type.toLowerCase() == 'залік') {
+                    grade2 = obj.grade < 60 ? 'не зараховано' : 'зараховано';
+                }
+
+                let $element = $(`
+                    <tr>
+                        <td>${crossNumber}</td>
+                        <td class="align_left">${obj.name}</td>
+                        <td>${obj.credits}/${obj.credits / 30}</td>
+                        <td>${obj.type}</td>
+                        <td>${grade2}</td>
+                        <td>${obj.grade}</td>
+                        <td>${grade3}</td>
+                    </tr>
+                `);
+                $table.append($element);
+            });
+
+            if(value.length > 0) {
+                grade /= value.length;
+                $element = $(`
+                    <tr>
+                        <td colspan="2" class="align_left table-grade">Середній бал успішності за семестр</td>
+                        <td class="table-grade">${grade.toFixed(2)}</td>
+                    </tr>
+                `);
+                $table.append($element);
+            }
+        });
+
+        $('.none').removeClass('none');
+    }
+
+    $('form').submit(function(event) {
+        event.preventDefault();
+
+        let $first = $('[name="first_name"]');
+        let $second = $('[name="second_name"]');
+        let $third = $('[name="third_name"]');
+        let $group = $('[name="group_name"]');
+
+        let first = $first.val().trim().toLowerCase();
+        let second = $second.val().trim().toLowerCase();
+        let third = $third.val().trim().toLowerCase();
+        let group = $group.val().trim().replaceAll(' ', '').toLowerCase();
+
+        let value = first + second + third + group;
+
+        // console.log(value);
+
+        value = md5(value);
+        // console.log(value);
+
+        // console.log(window.grades[value]);
+
+        if(window.grades[value] == null) {
+            alert("Студент з такими даними не знайдено!")
+            return;
+        }
+
+        fillTable(window.grades[value]);
+        $(this).addClass('none');
+    });
+
+    
+});
